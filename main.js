2d6a7bb368f797f0a10c8ea4b445991d21603721
Gurjot Singh <Laugh_and_drive@Gurjots-MacBook-Pro.local>
2016-01-08 23:51:01 -0800

    second commit

--- main.js
+++ main.js
@@ -18,23 +18,58 @@ function functionChoose(event){
 }
 
 function containClick(event){
-	$(event.target).css('background-color', '#d00');
+	
 	console.log('In Contain click')
+
+	if ($(event.target).hasClass('container')){
+		$firstContainer= $(event.target);
+
+
+	} else  if ($(event.target).hasClass("disk")){
+
 	$firstContainer = ($(event.target).parent('.container'));
+
+
+	}
+
 	console.log("firstContainer:", $firstContainer);
 	$pieceToMove = $firstContainer.find(':first-child');
+	$pieceToMove.css('background-color', '#d00');
 	pieceSelected = true
 	// $('.container').on('click' , diskmove);
 
 }
 
 function diskMove(event){
-	console.log("In Move Function", $firstContainer.parent());
-	if (event.target !== $firstContainer.parent){
-		$pieceToMove.remove();
+	console.log("In Move Function", $firstContainer);
+	console.log($(event.target).hasClass('container'))
+	var $targetContainer;
+	if ($(event.target).hasClass('container')){
+		$targetContainer= $(event.target);
+
+
+	} else  if ($(event.target).hasClass("disk")){
+
+	$targetContainer = ($(event.target).parent('.container'));
+	
 
 	}
-    $(event.target).prepend($pieceToMove);
+	console.log('second:', $targetContainer.find(':first-child').text());
+	console.log('first:', $pieceToMove.text());
+	if( $pieceToMove.text() < $targetContainer.find(':first-child').text()){
+
+		$targetContainer.prepend($pieceToMove);
+
+	 }
+	 console.log($targetContainer.children().length);
+	 if( $targetContainer.children().length === 0){
+	 	 $targetContainer.prepend($pieceToMove);
+
+	 }
+	if ($('.contain3').children().length ===3 ){
+		alert("You Win !!! ")
+	}
+    
     pieceSelected = false;
 }
 
